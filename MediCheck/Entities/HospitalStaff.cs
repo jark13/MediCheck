5e36ee881d06c7a8869a87586708b0fd3345f963
Jaroslaw Krzywanski <jkrzywanski@tlen.pl>
2024-01-03 21:38:58 +0100

    MediCheck - poprawak 2 - usunięcie Id z HospitalStaff

--- MediCheck/Entities/HospitalStaff.cs
+++ MediCheck/Entities/HospitalStaff.cs
@@ -1,9 +1,7 @@
 ﻿namespace MediCheck.Entities
 {
     public class HospitalStaff : EntityBase
-    {
-        //public int Id { get; set; }
-
+    {       
         public string? FirstName { get; set; }
 
         public override string ToString() => $"Id: {Id}, FirstName: {FirstName}";
